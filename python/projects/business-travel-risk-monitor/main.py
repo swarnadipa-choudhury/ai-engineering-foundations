d1ff112b449083e4ca8bffa4af4f091f377007ad
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2026-09-09 19:31:57 +0530

    feat: add and print sample trip data

--- python/projects/business-travel-risk-monitor/main.py
+++ python/projects/business-travel-risk-monitor/main.py
@@ -1,1 +1,24 @@
-print("Hi")
+sample_trip = {
+    "trip_id": 1234,
+    "employee_name": "Swarna",
+    "destination": {
+        "city": "Kolkata",
+        "country": "India"
+    },
+    "travel_date": "11-09-2026",
+    "return_date": "21-09-2026",
+    "purpose": "festival"
+    
+}
+
+
+print('...................')
+print('*****Trip details*****') 
+print('...................')
+for (key, value) in sample_trip.items():
+    if isinstance(value, dict):
+        print("Destination: ")
+        for (k, v) in value.items():
+            print(f"{k}: {v}")
+    else:
+        print(f"{key} : {value}")
